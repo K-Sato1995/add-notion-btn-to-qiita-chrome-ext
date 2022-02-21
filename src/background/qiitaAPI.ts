9ee29b16ab8830123e0f783d85880c910b0acefa
Katsuki <Katsuki_Sato0130@icloud.com>
2022-02-21 16:08:11 +0900

    Refactor

--- src/background/qiitaAPI.ts
+++ src/background/qiitaAPI.ts
@@ -22,22 +22,10 @@ export class QiitaAPIClient {
     }
 
     const userInfo = await response.json();
-    console.log(userInfo)
     return userInfo;
   }
 
   checkToken() {
     return this.accessToken;
   }
-}
-
-// set your token
-// const client = new QiitaAPIClient('14f975282f1fd61e769f8feb607509c1a8a61b9c');
-
-// Qiita.setEndpoint('https://qiita.com');
-
-export const fetchUser = () => {
-
-  // set your token
-  const client = new QiitaAPIClient('14f975282f1fd61e769f8feb607509c1a8a61b9c');
 }
