fe0a0335ec8c0057f32316c4c8d734fbfaa689ae
Katsuki <Katsuki_Sato0130@icloud.com>
2022-02-21 20:02:30 +0900

    Update test

--- src/background/__tests__/qiitaAPI.test.ts
+++ src/background/__tests__/qiitaAPI.test.ts
@@ -1,13 +1,12 @@
 import { QiitaAPIClient } from '../qiitaAPI'
 
 const token = 'token1234';
+const client = new QiitaAPIClient(token);
 
 test('Initialization', () => {
-  const client = new QiitaAPIClient(token);
   expect(client.accessToken).toBe(token);
 });
 
-
-test('two plus two is four', () => {
-  expect(2 + 2).toBe(4);
+test('CheckToken()', () => {
+  expect(client.checkToken()).toBe(token);
 });
