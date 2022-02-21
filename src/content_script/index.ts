19d0bdd750c95a7dfef78ced731d7367fda3be50
Katsuki <Katsuki_Sato0130@icloud.com>
2022-02-21 21:05:12 +0900

    Move notinoBtn funcs to a file

--- src/content_script/index.ts
+++ src/content_script/index.ts
@@ -1,16 +1,5 @@
 import type { RequestResult } from "../types";
-import { MESSAGE_KEY_INSERT_TO_DB, QIITA_ARTICLE_TITLE_TAG, QIITA_ARTICLE_TAG_HREF, QIITA_POSTED_DATE_TAG } from '../consts'
-
-// Access to the DOMs and get necessary resources
-const qiitaTitle = document.getElementsByTagName(QIITA_ARTICLE_TITLE_TAG)[0].innerText
-const qiitaTags = document.querySelectorAll(`[href*="${QIITA_ARTICLE_TAG_HREF}"]`);
-const postedDate = document.getElementsByTagName(QIITA_POSTED_DATE_TAG)[0].innerHTML;
-
-let tagTexts: string[] = []
-
-for(let i = 0; i < qiitaTags.length; i++) {
-  tagTexts.push(qiitaTags[i].innerHTML)
-}
+import { appendNotionBtnToQiita } from './notionBtn'
 
 // Listen to the msg from background script
 chrome.runtime.onMessage.addListener((response: RequestResult, _sender, _sendResponse) => {
@@ -22,11 +11,4 @@ chrome.runtime.onMessage.addListener((response: RequestResult, _sender, _sendRes
     return true
 });
 
-const currentURL = document.location.href;
-const logo = document.createElement("button"); 
-logo.setAttribute("id", "notion-btn-to-qiita-btn");
-logo.innerText="N";
-logo.onclick = () => { 
-    chrome.runtime.sendMessage({ msg: MESSAGE_KEY_INSERT_TO_DB, currentURL, qiitaTitle, tagsText: tagTexts.join(' '), postedDate });
-}
-document.body.appendChild(logo);
+appendNotionBtnToQiita()
