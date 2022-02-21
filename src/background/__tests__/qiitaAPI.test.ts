36d43aa80c7a48ea05b7b5fc38edd4da771fb404
Katsuki <Katsuki_Sato0130@icloud.com>
2022-02-21 20:41:50 +0900

    Update jest

--- src/background/__tests__/qiitaAPI.test.ts
+++ src/background/__tests__/qiitaAPI.test.ts
@@ -2,11 +2,27 @@ import { QiitaAPIClient } from '../qiitaAPI'
 
 const token = 'token1234';
 const client = new QiitaAPIClient(token);
+const userInfo = { ok: true, name: "k-sato", description: "k-sato" }
+
+// https://benjaminjohnson.me/mocking-fetch
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    ok: true,
+    json: () => Promise.resolve(userInfo),
+  })
+) as jest.Mock;
+
 
 test('Initialization', () => {
   expect(client.accessToken).toBe(token);
 });
 
 test('CheckToken()', () => {
-  expect(client.checkToken()).toBe(token);
+  expect(client.checkToken()).toBe(token)
+});
+
+test('fetchUserInfo()', async () => {
+  const result = await client.fetchUser('randomId')
+  expect(result).toBe(userInfo);
+  expect(fetch).toHaveBeenCalledTimes(1);
 });
