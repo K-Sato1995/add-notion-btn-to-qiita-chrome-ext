71ecd61cea6e0b0550b8fd8c22409231903e2c12
Katsuki <Katsuki_Sato0130@icloud.com>
2022-02-22 16:43:38 +0900

    Display name and description

--- src/content_script/userProfile.ts
+++ src/content_script/userProfile.ts
@@ -1,28 +1,38 @@
 import { QIITA_OPTIONS_CLASS_NAME } from '../consts'
-import type { UserProfileResponse } from '../types'
+import type { Qiita, UserProfileResponse } from '../types'
 
 // https://stackoverflow.com/questions/36798005/append-multiple-items-in-javascripts
-export const insertUserProfile = (desc) => {
+export const insertUserProfile = (userData: Partial<Qiita.User>) => {
   const mainBody = document.getElementsByClassName(QIITA_OPTIONS_CLASS_NAME)[0]
 
   const userContainer = document.createElement("div"); 
   userContainer.setAttribute("id", "user-profile-container");
-  userContainer.innerText=desc;
+  // userContainer.innerText = description;
   // Construct user's profile.
-  appendChildren(userContainer,createChildren())
+  appendChildren(userContainer,createChildren(userData))
   mainBody.prepend(userContainer)
 }
 
-const createChildren = () => {
+const createChildren = (userData: Partial<Qiita.User>) => {
+  const { id, name, description } = userData;
   // UserProfileTop
   const userProfileTop = document.createElement('div')
   userProfileTop.setAttribute("id", "user-profile-top");
 
+  // Create doms
   const userName = document.createElement('div')
+  const userDescription = document.createElement('div')
+ 
+  // Set Attributes
   userName.setAttribute("id", "user-name");
-  userName.innerText="UserNamee";
+  userDescription.setAttribute("id", "user-description");
 
-  userProfileTop.appendChild(userName)
+  // Set Values
+  userName.innerText = name;
+  userDescription.innerText = description;
+
+  appendChildren(userProfileTop, [userName, userDescription])
+
   return [userProfileTop]
 }
 
@@ -35,7 +45,7 @@ const appendChildren = (parentNode: HTMLElement, domNodes: HTMLElement[]) => {
 export const handleUserProfileResponse = (response: UserProfileResponse) => {
   if (response.type == 'OK') {
     const { userData } = response;
-    insertUserProfile(userData.description)
+    insertUserProfile(userData)
   } else {
     alert(response.msg)
   }
--- src/popup.ts
+++ src/popup.ts
@@ -2,7 +2,7 @@ import Options from "./components/Options.svelte";
 import type { IStorage } from "./types";
 
 chrome.storage.sync.get({ notionAPIToken: '', dbID: '' }  as IStorage, ({ notionAPIToken, dbID }: IStorage) => {
-    const app = new Options({
+    new Options({
         target: document.body,
         props: { notionAPIToken, dbID },
     });
